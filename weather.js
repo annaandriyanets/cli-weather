48c03646e1e6e2c0ec7618d640a09d9b71270732
Anna <annaandriyanets0@gmail.com>
2022-03-01 19:20:25 +0300

    finished arg helper

--- weather.js
+++ weather.js
@@ -1,8 +1,17 @@
-#!/usr/bin/env node
 import {getArgs} from './helpers/args.js'
 const initCLI = () => {
     const args = getArgs(process.argv)
     console.log(args)
+    if (args.h) {//выводит help
+
+    }
+    if (args.s) {//сохранить город
+
+    }
+    if (args.t) {//сохранить токен
+
+    }
+    //вывести погоду
 };
 
 initCLI();
